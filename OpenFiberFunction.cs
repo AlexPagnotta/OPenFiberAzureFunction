110c9acebd95a62d58870e0cb13dee7abd7adf10
Alex PAgnotta <alex.pagnotta@outlook.it>
2020-09-08 06:51:45 +0200

    Initial Use of AngleSharp

--- OpenFiberFunction.cs
+++ OpenFiberFunction.cs
@@ -2,17 +2,28 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using AngleSharp;
+using AngleSharp.Html.Parser;
 
 namespace AlexPagnotta.Function
 {
     public static class OpenFiberFunction
     {
         [FunctionName("OpenFiberFunction")]
-        public static void Run([TimerTrigger("0 0 5 * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 0 5 * * *")]TimerInfo myTimer, ILogger log)
         {
-            //URL https://openfiber.it/verifica-copertura/umbria/terni/fabro/?egon_id_for_civic=380100091821085&egon_id_for_address=38000648389&cityID=4805&street=via%20vittorio%20veneto&civic=31&cap=05015
+            var url = "https://openfiber.it/verifica-copertura/umbria/terni/fabro/?egon_id_for_civic=380100091821085&egon_id_for_address=38000648389&cityID=4805&street=via%20vittorio%20veneto&civic=31&cap=05015"
             
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            // Load default configuration
+            var config = Configuration.Default.WithDefaultLoader();
+            // Create a new browsing context
+            var context = BrowsingContext.New(config);
+            // This is where the HTTP request happens, returns <IDocument> that // we can query later
+            var document = context.OpenAsync(url).Result;
+            // Log the data to the console
+            log.LogInformation(document.DocumentElement.OuterHtml);
         }
     }
 }
