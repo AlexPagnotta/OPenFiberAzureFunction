2107251ad47338ef94a5ca6222779d4551914313
Alex <alex.pagnotta@outlook.it>
2020-09-09 07:36:17 +0200

    Moved variables to Settings json

--- OpenFiberFunction.cs
+++ OpenFiberFunction.cs
@@ -12,7 +12,10 @@ namespace AlexPagnotta.Function
         [FunctionName("OpenFiberFunction")]
         public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            var url = @"https://openfiber.it/verifica-copertura/umbria/terni/fabro/?egon_id_for_civic=380100091821085&egon_id_for_address=38000648389&cityID=4805&street=via%20vittorio%20veneto&civic=31&cap=05015";
+            var url = Environment.GetEnvironmentVariable("OpenFiberUrl");
+            var FTTHCoverageString = Environment.GetEnvironmentVariable("FTTHCoverageString");
+            var FWACoverageString = Environment.GetEnvironmentVariable("FWACoverageString");
+            var NOCoverageString = Environment.GetEnvironmentVariable("NOCoverageString");
             
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
@@ -26,8 +29,27 @@ namespace AlexPagnotta.Function
             var connectionStatusElementParent = document.QuerySelector(".search-by-address.covered-type");
             var connectionStatusString = connectionStatusElementParent.QuerySelector("strong");
 
+            var coverageString = connectionStatusString.InnerHtml;
+
             // Log the data to the console
-            log.LogInformation(connectionStatusString.InnerHtml);
+            log.LogInformation(coverageString);
+
+            var emailString = "";
+
+            if(coverageString == FTTHCoverageString){
+                emailString = "Finally FTTH!";
+            }
+            else if(coverageString == FWACoverageString){
+                emailString = "FWA, Nothing Changed";
+            }
+            else if(coverageString == NOCoverageString){
+                emailString = "What, No Coverage Now?";
+            }
+            else{
+                //Exception;
+            }
+
+            
         }
     }
 }
