438db25b2fd1468414c8b183ad1639d4d2d10bcd
Alex <alex.pagnotta@outlook.it>
2020-09-10 21:09:55 +0200

    Added SendGrid

---
+++ startup.cs
@@ -0,0 +1,12 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+
+namespace AlexPagnotta.Function
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+        }
+    }
+}
