d7ea9e730ba9b42d3ac1d89e83075bb69cd0da31
Alex <alex.pagnotta@outlook.it>
2020-09-15 07:00:22 +0200

    Option Pattern Implementation Ended

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -1,8 +1,25 @@
+using System.Collections.Generic;
+
 public class Settings
-{
-    public Settings()
-    {
-    }
-    
+{   
     public string OpenFiberUrl { get; set; }
+    public string FWACoverageStringIdentifier { get; set; }
+    public string FTTHCoverageStringIdentifier { get; set; }
+    public string NOCoverageStringIdentifier { get; set; }
+    public string AngleSharp_ParentElementIdentifier { get; set; }
+    public string AngleSharp_CoverageStringElementIdentifier { get; set; }
+
+}
+
+public class SendGridSettings
+{
+    public string SendGrid_API_KEY { get; set; }
+    public string SendGrid_Sender { get; set; }
+    public List<SendGridReceiver> SendGrid_Receivers { get; set; }
+}
+
+public class SendGridReceiver 
+{
+    public string Email { get; set; }
+    public string Name { get; set; }
 }
